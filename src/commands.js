ef60ee94097e91e719e1dc09541d352160d43604
iamjexcdevvv <85010916+iamjexcdevvv@users.noreply.github.com>
2026-01-10 20:32:55 +0800

    feat: package uninstallation

--- src/commands.js
+++ src/commands.js
@@ -2,7 +2,7 @@ const { Command } = require("commander");
 const program = new Command();
 
 const initSPM = require("./spm-init");
-const installPackage = require("./package-management");
+const { installPackage, uninstallPackage } = require("./dependency");
 
 program
 	.name("SAMP Package Manager")
@@ -12,9 +12,16 @@ program
 program
 	.command("install")
 	.alias("i")
-	.description("Install a specified SAMP library")
-	.argument("<package>", "A SAMP package to install")
+	.description("Install a specified dependency")
+	.argument("<package>", "A dependency to install")
 	.action((package) => installPackage(package));
+
+program
+	.command("uninstall")
+	.alias("un")
+	.description("Uninstall a specified dependency")
+	.argument("<package>", "A dependency to uninstall")
+	.action((package) => uninstallPackage(package));
 
 program
 	.command("init")
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const fs = require("fs");
+
 function isValidPackageFormat(package) {
 	const regex = /^([a-zA-Z0-9_.-]+)\/([a-zA-Z0-9_.-]+)$/;
 	return regex.test(package);
@@ -7,4 +10,28 @@ function formatJSON(obj, indent, replacer = null) {
 	return JSON.stringify(obj, replacer, indent);
 }
 
-module.exports = { isValidPackageFormat, formatJSON };
+function isPawnConfigFileFound() {
+	const configFilePath = path.join(process.cwd(), "pawn.json");
+	return fs.existsSync(configFilePath);
+}
+
+function extractPackageNameAndOwner(package) {
+	const separatorIdx = package.indexOf("/");
+	const username = package.slice(0, separatorIdx);
+	const repo = package.slice(separatorIdx + 1);
+
+	return { username, repo };
+}
+
+function updatePawnConfigFile(configData) {
+	const formattedStr = formatJSON(configData, 4);
+	fs.writeFileSync("pawn.json", formattedStr);
+}
+
+module.exports = {
+	isValidPackageFormat,
+	formatJSON,
+	isPawnConfigFileFound,
+	extractPackageNameAndOwner,
+	updatePawnConfigFile,
+};
