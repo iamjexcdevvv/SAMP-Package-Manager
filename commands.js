f4c4a704988986433054b6afb9213dba53148053
iamjexcdevvv <85010916+iamjexcdevvv@users.noreply.github.com>
2026-01-10 12:41:55 +0800

    feat: SPM initialization command

--- commands.js
+++ commands.js
@@ -1,7 +1,7 @@
 const { Command } = require("commander");
 const program = new Command();
 
-const installPackage = require("./index");
+const { installPackage, initSPM } = require("./index");
 
 program
 	.name("SAMP Package Manager")
@@ -13,5 +13,11 @@ program
 	.description("Install a specified SAMP library")
 	.argument("<package>", "A SAMP package to install")
 	.action((package) => installPackage(package));
+
+program
+	.command("init")
+	.description("Initialize SAMP package manager")
+	.option("-y, --yes", "Yes to all")
+	.action((options) => initSPM(options));
 
 module.exports = program;
--- index.js
+++ index.js
@@ -4,18 +4,115 @@ require("dotenv").config({ quiet: true });
 const config = require("./spm-config.json");
 const personalToken = config.github_token || process.env.GITHUB_TOKEN;
 
+// Third-Party modules
 const { Octokit } = require("octokit");
 const octokit = new Octokit({ auth: personalToken });
 
+const unzipper = require("unzipper");
+
+const simpleGit = require("simple-git");
+const git = simpleGit();
+
+// Core Modules
+const os = require("os");
 const path = require("path");
 const { pipeline } = require("stream/promises");
 const fs = require("fs");
-const unzipper = require("unzipper");
+
+const prompts = require("prompts");
+
+const USERNAME = os.userInfo().username;
+const PACKAGENAME = path.parse(process.cwd()).base;
+
+const promptQuestions = [
+	{
+		type: "text",
+		name: "username",
+		message:
+			"Your Name - If you plan to release, must be your GitHub username.",
+		initial: USERNAME,
+	},
+	{
+		type: "text",
+		name: "packageName",
+		message:
+			"Package Name - If you plan to release, must be the GitHub project name.",
+		initial: PACKAGENAME,
+	},
+	{
+		type: "confirm",
+		name: "devFiles",
+		message: "Add a .gitignore and .gitattributes files? (y/n)",
+		initial: false,
+	},
+	{
+		type: "confirm",
+		name: "readmeFile",
+		message: "Add a README.md file? (y/n)",
+		initial: false,
+	},
+	{
+		type: "confirm",
+		name: "gitinit",
+		message: "Initialise a git repository? (y/n)",
+		initial: false,
+	},
+];
+
+function isValidPackageFormat(package) {
+	const regex = /^([a-zA-Z0-9_.-]+)\/([a-zA-Z0-9_.-]+)$/;
+	return regex.test(package);
+}
+
+async function initSPM(options) {
+	if (options.yes) {
+		// The user skips the prompt
+		return;
+	}
+
+	try {
+		const onCancel = (prompt) => {
+			console.log("ERROR: interrupt");
+			process.exit(1);
+		}
+
+		// Prompt for entry point this is used for compiling the gamemode
+		// const entryPoint = await askUser(
+		// 	"Choose an entry point - this is the file that is passed to the compiler."
+		// );
+
+		const response = await prompts(promptQuestions, {onCancel});
+
+		if (response.devFiles) {
+			fs.writeFileSync(".gitignore", "");
+			fs.writeFileSync(".gitattributes", "");
+		}
+
+		if (response.readmeFile) {
+			fs.writeFileSync("README.md", "");
+		}
+
+		if (response.gitinit) {
+			await git.init();
+			console.log("SPM: Initialized git repository");
+		}
+
+		const spmConfig = {
+			user: response.username,
+			repo: response.packageName
+		}
+
+		fs.writeFileSync("pawn.json", JSON.stringify(spmConfig, null, 4));
+		console.log("SPM: initialized");
+	} catch (error) {
+		console.log(error);
+	}
+}
 
 async function installPackage(package) {
-	const regex = /^([a-zA-Z0-9_.-]+)\/([a-zA-Z0-9_.-]+)$/;
+	const isValid = isValidPackageFormat(package);
 
-	if (!regex.test(package)) {
+	if (!isValid) {
 		console.error("Error: Invalid format. Use username/repo");
 		process.exit(1);
 	}
@@ -53,9 +150,9 @@ async function installPackage(package) {
 		const targetFolder = path.join(sampModulesDir, repo);
 
 		if (!fs.existsSync(cachedPackagePath)) {
-			const zipUrl = `https://github.com/${username}/${repo}/archive/refs/heads/${branch}.zip`;
+			const downloadURL = `https://github.com/${username}/${repo}/archive/refs/heads/${branch}.zip`;
 
-			const response = await fetch(zipUrl);
+			const response = await fetch(downloadURL);
 
 			if (!response.ok) {
 				console.error("Error: Can't download the library");
@@ -84,8 +181,10 @@ async function installPackage(package) {
 	}
 }
 
-module.exports = installPackage;
+module.exports = {
+	initSPM,
+	installPackage,
+};
 
 const program = require("./commands");
-
 program.parse();
